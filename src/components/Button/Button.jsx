47e3713fba9c56a9741be4d845e3d5049bee55f0
nadiachenko <97396844+nadiachenko@users.noreply.github.com>
2024-01-09 21:02:44 +0200

    thrd

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@
 import css from 'components/Button/button.module.css'
 export const Button = ({ loadMore }) => {
 
-    return (<button onClick={() => loadMore()} className={css.Button}>
+    return (<button onClick={()=>loadMore()} className={css.Button}>
         Load More
   </button>);
 
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Button } from 'components/Button/Button';
 import { Loader } from "components/Loader/Loader";
@@ -13,28 +13,29 @@ const ImageGallery = ({ loadMore, query, page }) => {
   const [largeImageURL, setLargeImageURL] = useState('');
   const [prevQuery, setPrevQuery] = useState(null);
 
+//console.log("Prevquetry", prevQuery)
 
-  const fetchSearch = async () => {
-    try {
-      setLoading(true);
-      setError(null);
 
-      if (prevQuery !== query) {
-        setHits([]);
-      }
-      const response = await fetch(`https://pixabay.com/api/?q=${query}&page=${page}&key=40934415-dfd7c79ea7303db44ba7dd17c&image_type=photo&orientation=horizontal&per_page=12`);
-      if (!response.ok) {
-        throw new Error(`Oops... Something went wrong`);
-      }
-      const hitsData = await response.json();
-      if (hitsData.hits.length === 0) {
-        throw new Error(`No images found for query: ${query}`);
-      }
-      setHits(existingHits => [...existingHits, ...hitsData.hits])
+const fetchSearch = async () => {
+  try {
+    setLoading(true);
+    setError(null);
+
+    if (prevQuery !== query) {
+      setHits([]);
     }
-    catch (error) { setError(error) }
-    finally { setLoading(false) }
-  };
+    const response = await fetch(`https://pixabay.com/api/?q=${query}&page=${page}&key=40934415-dfd7c79ea7303db44ba7dd17c&image_type=photo&orientation=horizontal&per_page=12`);
+    if (!response.ok) {
+  throw new Error(`Oops... Something went wrong`);
+    }
+    const hitsData = await response.json();
+    if (hitsData.hits.length === 0) {
+  throw new Error(`No images found for query: ${query}`);
+    }
+    setHits(existingHits => [...existingHits, ...hitsData.hits])} 
+    catch (error) {setError(error)} 
+    finally {setLoading(false)}
+};
 
   useEffect(() => {
     if (!query) {
@@ -46,7 +47,7 @@ const ImageGallery = ({ loadMore, query, page }) => {
 
 
   const toggleModal = (largeImageURL) => {
-   
+   // console.log(ShowModal)
     setShowModal((prevShowModal) => !prevShowModal)
     setLargeImageURL(largeImageURL);
   };
